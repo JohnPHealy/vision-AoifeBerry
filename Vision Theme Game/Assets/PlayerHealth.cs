c7a3b7186c6ed4590133b4d867cb81cd811f8a29
AoifeBerry <71941986+AoifeBerry@users.noreply.github.com>
2021-04-16 17:55:15 +0100

    player has working health

---
+++ Vision Theme Game/Assets/PlayerHealth.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+
+
+{
+
+    public int maxHealth = 10;
+    public int currentHealth;
+    public int damage = 1;
+    public healthBar healthBar;
+    public Transform SpawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+
+    public void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            currentHealth -= damage;
+            healthBar.SetHealth(currentHealth);
+        }
+
+     
+        
+    }
+
+    void Update()
+    {
+        if (currentHealth <= 0)
+        {
+
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
