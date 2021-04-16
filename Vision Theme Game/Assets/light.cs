0149b0ee2e4858d056876247c3a3e47dd4dca0a3
AoifeBerry <71941986+AoifeBerry@users.noreply.github.com>
2021-04-16 16:59:23 +0100

    light fades over time

--- Vision Theme Game/Assets/light.cs
+++ Vision Theme Game/Assets/light.cs
@@ -9,12 +9,19 @@ public class light : MonoBehaviour
     public float lightIntensity = 2;
     public float addRange = 5;
     public float addIntens = 2;
-    
+    public float fallOff = - 1f;
+    public float fallOff2 = -0.5f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-     
+        
+        StartCoroutine(coroutineA());
+
     }
+
+ 
 
     // Update is called once per frame
     void Update()
@@ -22,8 +29,10 @@ public class light : MonoBehaviour
         GetComponent<Light>().range = lightAmount;
         GetComponent<Light>().intensity = lightIntensity;
 
-        
+
     }
+
+  
 
     public void OnCollisionEnter(Collision col)
     {
@@ -31,9 +40,29 @@ public class light : MonoBehaviour
         {
             lightAmount += addRange;
             lightIntensity += addIntens;
-        } 
-       
+        }
+    
+
     }
 
- 
+    IEnumerator coroutineA()
+    {
+        // wait for 1 second
+        Debug.Log("coroutineA created");
+        yield return new WaitForSeconds(.5f);
+        yield return StartCoroutine(coroutineB());
+        Debug.Log("coroutineA running again");
+    }
+
+    IEnumerator coroutineB()
+    {
+        Debug.Log("coroutineB created");
+        yield return new WaitForSeconds(1f);
+        lightAmount -= fallOff;
+        lightIntensity += fallOff2;
+        yield return StartCoroutine(coroutineA());
+        
+        Debug.Log("coroutineB enables coroutineA to run");
+    }
+
 }
